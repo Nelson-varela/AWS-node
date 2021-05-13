62f1c29ab222c8ff490cb35e016d642f879202d4
Nelson <xnelsonvsx@hotmail.com>
2021-05-13 08:13:47 -0500

    text modified

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express')
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello world with express')
-})
+app.get('/', (req, res) =>  res.send('<h1>Express con html<h1>'))
 
 
 
