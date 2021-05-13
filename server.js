fba0d505aaf472433442d17d66389692523e015e
Nelson <xnelsonvsx@hotmail.com>
2021-05-13 08:24:44 -0500

    text mod

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express();
 
-app.get('/', (req, res) =>  res.send('<h1>Express con html<h1>'))
+app.get('/', (req, res) =>  res.send('<h1>Express con HTML, y seré un programador muy completo!!! ya verán<h1>'))
 
 
 
